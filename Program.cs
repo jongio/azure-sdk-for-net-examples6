7ae74914cdceb336df62521b32de7f1064e4f9a6
Jon Gallant <jong@microsoft.com>
2021-05-13 10:15:41 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_sdk_for_net_examples6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
